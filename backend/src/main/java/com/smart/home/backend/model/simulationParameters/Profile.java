80c316de9b91cc827cc6e0d4db647de813e60f97
Juan Hoyos <juanse.jsh@gmail.com>
2020-10-13 14:40:13 -0400

    Profile class created

---
+++ backend/src/main/java/com/smart/home/backend/model/simulationParameters/Profile.java
@@ -0,0 +1,17 @@
+package com.smart.home.backend.model.simulationParameters;
+
+import com.smart.home.backend.model.ModelObject;
+import com.smart.home.backend.constant.Role;
+import lombok.experimental.SuperBuilder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@SuperBuilder
+public class Profile extends ModelObject{
+    private String name;
+    private String username;
+    private String password;
+    private Role role;
+}
