f8ff9ed60cbecc60c8ecdac8d41d4a1828d1bcc0
RobertoNittolo <nitrobert@yahoo.com>
2020-10-18 20:35:55 -0400

    Added Person Model

---
+++ backend/src/main/java/com/smart/home/backend/model/houselayout/Person.java
@@ -0,0 +1,16 @@
+package com.smart.home.backend.model.houselayout;
+
+import lombok.Getter;
+import lombok.Setter;
+import com.smart.home.backend.model.ModelObject;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * Class for a people in a room
+ */
+@Getter
+@Setter
+@SuperBuilder
+public class Person extends ModelObject {
+	
+}
