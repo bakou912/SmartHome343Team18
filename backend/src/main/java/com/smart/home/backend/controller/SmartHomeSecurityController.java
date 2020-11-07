62183d57c455b106a3ea23075fbfc913ea0ef1e2
RobertoNittolo <nitrobert@yahoo.com>
2020-11-06 23:18:27 -0500

    Added route to get away mode state

--- backend/src/main/java/com/smart/home/backend/controller/SmartHomeSecurityController.java
+++ backend/src/main/java/com/smart/home/backend/controller/SmartHomeSecurityController.java
@@ -8,6 +8,7 @@ import com.smart.home.backend.model.smarthomesecurity.Security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -59,4 +60,8 @@ public class SmartHomeSecurityController {
 		return new ResponseEntity<>(this.getSecurity().getAlertAuthoritiesTime(), HttpStatus.OK);
 	}
 
+	@GetMapping("context/awayMode/state")
+	public ResponseEntity<Boolean> getAwayMode(){
+		return new ResponseEntity<>(this.getSecurity().getAwayMode(), HttpStatus.OK);
+	}
 }
