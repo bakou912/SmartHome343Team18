92ef63dafe5a936d781becfacfc6323c43ecea66
RobertoNittolo <nitrobert@yahoo.com>
2020-11-06 23:22:04 -0500

    Modified SHP module

--- frontend/src/component/modules/SHPModule.js
+++ frontend/src/component/modules/SHPModule.js
@@ -4,37 +4,36 @@ import "../../style/Modules.css";
 import Switch from "react-switch";
 import { Container } from "react-bootstrap";
 export default class SHPModule extends React.Component {
-  constructor() {
-    super();
-    this.state = { checked: false };
-    this.changeHandler = this.changeHandler.bind(this);
-  }
+    constructor() {
+        super();
+        this.state = { checked: SmartHomeSecurityService.getAwayModeState };
+        this.changeHandler = this.changeHandler.bind(this);
+    }
 
-  changeHandler(checked) {
-    this.setState({ checked });
-    SmartHomeSecurityService.toggleAwayMode(checked);
-  }
+    changeHandler(checked) {
+        this.setState({ checked });
+        SmartHomeSecurityService.toggleAwayMode(checked);
+    }
 
-  render() {
-    return (
-      <Container>
-        <div
-          className="Module"
-          style={{
-            display: "flex",
-            justifyContent: "left",
-            alignItems: "left",
-          }}
-        >
-          <label>
-            <span>Away Mode</span>
-            <Switch
-              onChange={this.changeHandler}
-              checked={this.state.checked}
-            />
-          </label>
-        </div>
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <Container>
+                <div className="Module"
+                    style={{
+                    display: "flex",
+                    justifyContent: "left",
+                    alignItems: "left",
+                }}
+                >
+                <label>
+                    <span>Away Mode</span>
+                        <Switch
+                            onChange={this.changeHandler}
+                            checked={this.state.checked}
+                        />
+                </label>
+                </div>
+            </Container>
+        );
+    }
 }
--- frontend/src/service/SmartHomeSecurityService.js
+++ frontend/src/service/SmartHomeSecurityService.js
@@ -1,12 +1,15 @@
 import httpClient from "./HttpClient";
 
 class SmartHomeSecurityService {
+    async toggleAwayMode(state) {
+        const path = `context/awayMode/${state}`;
+        return httpClient.put(path);
+     }
 
-  async toggleAwayMode(state) {
-    const path = `context/awayMode/${state}`;
-    return httpClient.put(path);
-  }
-
+    async getAwayModeState(){
+        const path ='context/awayMode/state'
+        return httpClient.get(path);
+    }     
 }
 
 export default new SmartHomeSecurityService();
