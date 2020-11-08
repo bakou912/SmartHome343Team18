add0534310f4b99b0fce45427e65fc84f06ff166
RobertoNittolo <nitrobert@yahoo.com>
2020-11-07 19:54:33 -0500

    Styling

--- frontend/src/component/modules/SHPModule.js
+++ frontend/src/component/modules/SHPModule.js
@@ -3,7 +3,6 @@ import SmartHomeSecurityService from "../../service/SmartHomeSecurityService";
 import "../../style/Modules.css";
 import Switch from "react-switch";
 import { Container } from "react-bootstrap";
-
 export default class SHPModule extends React.Component {
 
     constructor(props) {
@@ -29,21 +28,22 @@ export default class SHPModule extends React.Component {
     render() {
         return (
             <Container>
-                <div className="Module"
+                <Container 
+                    className="Module"
                     style={{
                     display: "flex",
                     justifyContent: "left",
                     alignItems: "left",
-                }}
+                    }}
                 >
-                <label>
-                    <span>Away Mode</span>
-                        <Switch
-                            onChange={this.changeHandler}
-                            checked={this.state.checked}
-                        />
-                </label>
-                </div>
+                    <Container>
+                        <span>Away Mode</span>
+                            <Switch
+                                onChange={this.changeHandler}
+                                checked={this.state.checked}
+                            />  
+                    </Container>
+                </Container>
             </Container>
         );
     }
